8c54e9a1b5523aa2ed4810cb2f8fe813ac36044d
pedro.octavio <pedrooctavio.dev@outlook.com>
2022-03-04 23:48:45 -0300

    interface documented

--- JwtAuthenticationServices/Interfaces/IJwtServices.cs
+++ JwtAuthenticationServices/Interfaces/IJwtServices.cs
@@ -1,6 +1,14 @@
 ﻿namespace JwtAuthenticationServices.Interfaces;
 
+/// <summary>
+/// provides util methods for jwts
+/// </summary>
 public interface IJwtServices
 {
+    /// <summary>
+    /// generate a jwt
+    /// </summary>
+    /// <param name="email">used to make jwt unique</param>
+    /// <returns></returns>
     string Generate(string email);
 }
