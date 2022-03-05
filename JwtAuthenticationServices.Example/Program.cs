22046633cbdfa141308086aba2413e609e494770
pedro.octavio <pedrooctavio.dev@outlook.com>
2022-03-04 23:43:05 -0300

    JwtAuthenticationServices DI implemented

--- JwtAuthenticationServices.Example/Program.cs
+++ JwtAuthenticationServices.Example/Program.cs
@@ -1,9 +1,58 @@
+using JwtAuthenticationServices.DI;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddJwtAuthenticationServices(builder.Configuration);
+
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Title = "Jwt Authentication Services Example",
+            Version = "1.0.0",
+            Description = "Example of Jwt Authentication Services implementation.",
+            Contact = new OpenApiContact
+            {
+                Name = "Pedro Octávio",
+                Email = "pedrooctavio.dev@outlook.com",
+                Url = new Uri("https://github.com/pedro-octavio")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "MIT",
+                Url = new Uri("https://github.com/pedro-octavio/JwtAuthenticationServices/blob/main/LICENSE")
+            }
+        });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Authorization header using the Bearer scheme."
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+    });
 
 var app = builder.Build();
 
@@ -12,6 +61,7 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
