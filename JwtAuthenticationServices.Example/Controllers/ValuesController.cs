703f39c10912bd75084f9e3f381cf129ad0067ad
pedro.octavio <pedrooctavio.dev@outlook.com>
2022-03-04 23:44:09 -0300

    ValuesController created implemented JwtAuthenticationServices

---
+++ JwtAuthenticationServices.Example/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using JwtAuthenticationServices.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthenticationServices.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController(IJwtServices jwtServices) =>
+            _jwtServices = jwtServices;
+
+        private readonly IJwtServices _jwtServices;
+
+        [HttpGet, Route("authenticated"), Authorize()]
+        public IActionResult Authorized() =>
+            NoContent();
+
+        [HttpGet, Route("without-authentication")]
+        public IActionResult WithoutAuthentication() =>
+            NoContent();
+
+        [HttpPost, Route("authenticate")]
+        public IActionResult WithoutAuthentication([FromBody] string email) =>
+            Ok(_jwtServices.Generate(email));
+    }
+}
